9acdb1f8ed571905e2d22b35914dde780b364c1f
theerachot <theerachot.la@rmuti.ac.th>
2024-02-18 14:04:10 +0700

    routes

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,51 +1,40 @@
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import LoginForm from "../layout/LoginForm";
 import RegisterForm from "../layout/RegisterForm";
 import useAuth from "../hooks/useAuth";
 import Header from "../layout/Header";
 import UserHome from "../layout/UserHome";
-import Reserve from "../layout/Reserve"
+import Reserve from "../layout/Reserve";
 import UserReserve from "../layout/UserReserve";
 import History from "../layout/History";
 
-const guestRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>
-        <Header />
-        <Outlet />
-      </>
-    ),
-    children: [
-      { index: true, element: <LoginForm /> },
-      { path: "/register", element: <RegisterForm /> },
-    ],
-  },
-]);
+const guestRoutes = (
+  <>
+    <Route path="/" element={<LoginForm />} />
+    <Route path="/register" element={<RegisterForm />} />
+  </>
+);
 
-const userRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>
-        <Header />
-        <Outlet />
-      </>
-    ),
-    children: [
-      { index: true, element: <UserHome/> },
-      { path: "/Reserve", element: <Reserve/> },
-      { path: "/user-home", element: <UserHome /> },
-      { path: "/user-reserve", element: <UserReserve /> },
-      { path: "/history", element: <History/> },
-    ],
-  },
-]);
+const userRoutes = (
+  <>
+    <Route path="/" element={<UserHome />} />
+    <Route path="/reserve" element={<Reserve />} />
+    <Route path="/user-home" element={<UserHome />} />
+    <Route path="/user-reserve" element={<UserReserve />} />
+    <Route path="/history" element={<History />} />
+  </>
+);
 
-
-export default function AppRouter() {
+const AppRouter = () => {
   const { user } = useAuth();
-  const finalRouter = user?.id ? userRouter : guestRouter;
-  return <RouterProvider router={finalRouter} />;
-}
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <Outlet />
+      <Routes>{user?.id ? userRoutes : guestRoutes}</Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
